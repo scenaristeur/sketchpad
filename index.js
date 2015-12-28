23cb99abbec034aec999c94e97c17c956b1dbe2a
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2015-12-28 22:38:29 +0100

    Get the listening port from an environment variable, conforming to the Heroku guidelines

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ var app = express();
 var server = http.Server(app);
 var io = socketio(server);
 
+// set port from the environment or fall back
+app.set('port', (process.env.PORT || 3000));
 // route to static files
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // listen
-server.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+server.listen(app.get('port'), function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('listening at http://%s:%s', host, port);
+    console.log('listening at http://%s:%s', host, port);
 });
